2e0c7f6426a11c1b0a8eed6a2decdea672669b0c
IamKhuk <khukumovkhusan@gmail.com>
2022-03-12 16:21:47 +0500

    Onboarding added images

--- lib/src/ui/splash/onboarding_screen.dart
+++ lib/src/ui/splash/onboarding_screen.dart
@@ -14,111 +14,145 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.white,
-      body: Column(
+      body: Stack(
         children: [
-          SizedBox(height: 40),
-          SvgPicture.asset(
-            'assets/images/elements.svg',
-            color: AppTheme.red,
-          ),
-          Spacer(),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.5,
-            width: MediaQuery.of(context).size.width - 32,
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 24,
-            ),
-            decoration: BoxDecoration(
-              color: AppTheme.red.withOpacity(0.6),
-              borderRadius: BorderRadius.circular(32),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 8),
-                Container(
-                  height: 51,
-                  width: 51,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.asset('assets/images/logo.png'),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 20),
+                      Image.asset(
+                        'assets/images/nurse.png',
+                        height: 270,
+                      ),
+                      SizedBox(width: 100),
+                    ],
                   ),
+                  Row(
+                    children: [
+                      SizedBox(width: 110),
+                      Image.asset(
+                        'assets/images/man_nurse.png',
+                        height: 300,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.48 + 16),
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(height: 40),
+              SvgPicture.asset(
+                'assets/images/elements.svg',
+                color: AppTheme.red,
+              ),
+              Spacer(),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.48,
+                width: MediaQuery.of(context).size.width - 32,
+                margin: EdgeInsets.all(16),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 24,
                 ),
-                Spacer(),
-                Row(
+                decoration: BoxDecoration(
+                  color: AppTheme.red.withOpacity(0.6),
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Text(
-                        'Complete Health Solution',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontFamily: AppTheme.fontFamily,
-                          fontSize: 36,
-                          color: AppTheme.white,
-                          height: 1.5,
-                        ),
+                    SizedBox(height: 8),
+                    Container(
+                      height: 51,
+                      width: 51,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset('assets/images/logo.png'),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 4),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Early protection for family health',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontFamily: AppTheme.fontFamily,
-                          fontSize: 16,
-                          color: AppTheme.white.withOpacity(0.8),
-                          height: 1.5,
+                    Spacer(),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Complete Health Solution',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontFamily: AppTheme.fontFamily,
+                              fontSize: 36,
+                              color: AppTheme.white,
+                              height: 1.5,
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
-                Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).popUntil(
-                          (route) => route.isFirst,
-                        );
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return HomeScreen();
-                            },
-                          ),
-                        );
-                      },
-                      child: Container(
-                        height: 64,
-                        width: 64,
-                        decoration: BoxDecoration(
-                          color: AppTheme.red,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Center(
-                          child: SvgPicture.asset(
-                            'assets/icons/right.svg',
-                            color: AppTheme.white,
-                            height: 24,
+                    SizedBox(height: 4),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Early protection for family health',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: AppTheme.fontFamily,
+                              fontSize: 16,
+                              color: AppTheme.white.withOpacity(0.8),
+                              height: 1.5,
+                            ),
                           ),
                         ),
-                      ),
+                      ],
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).popUntil(
+                              (route) => route.isFirst,
+                            );
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return HomeScreen();
+                                },
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 64,
+                            width: 64,
+                            decoration: BoxDecoration(
+                              color: AppTheme.red,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: SvgPicture.asset(
+                                'assets/icons/right.svg',
+                                color: AppTheme.white,
+                                height: 24,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
